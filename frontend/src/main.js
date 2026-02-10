38dccb57fea040b352768f4ef08ecc965d5b8669
javsrodz <javi.mora62@gmail.com>
2026-02-10 13:02:48 -0600

    install settings

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // Aquí puedes mostrar una notificación al usuario para que actualice la página
+    console.log('Nueva versión disponible. Por favor, actualiza la página.')
+  },
+  onOfflineReady() {
+    console.log('La aplicación está lista para funcionar sin conexión.')
+  }
+})
 
 createApp(App).mount('#app')
